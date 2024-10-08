b74ec4e2b200a071fbfb948aca227ff383695db3
vipulchaudhary31 <77533317+vipulchaudhary31@users.noreply.github.com>
2024-10-08 23:19:33 +0530

    Update script.js

--- search01/script.js
+++ search01/script.js
@@ -1,17 +1,15 @@
-var swiper = new Swiper('.swiper-container.two', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    effect: 'coverflow',
-    loop: false,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflow: {
-        rotate: 0,
-        stretch: 390,
-        depth: 150,
-        modifier: 1.5,
-        slideShadows: false,
-    },
-    direction: 'horizontal',
-    reverseDirection: true
-});
+var swiper = new Swiper( '.swiper-container.two', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		effect: 'coverflow',
+		loop: false,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflow: {
+			rotate:0		,
+			stretch: 390,
+			depth: 150,
+			modifier: 1.5,
+			slideShadows : false,
+		}
+} );
