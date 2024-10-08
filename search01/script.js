95604c8dd6dbfb44c663b41194cb5f7a44b5afac
vipulchaudhary31 <77533317+vipulchaudhary31@users.noreply.github.com>
2024-10-08 23:15:36 +0530

    Update script.js

--- search01/script.js
+++ search01/script.js
@@ -1,13 +1,11 @@
 var swiper = new Swiper('.swiper-container.two', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
     effect: 'coverflow',
     loop: false,
     centeredSlides: true,
     slidesPerView: 'auto',
-    coverflowEffect: {
+    coverflow: {
         rotate: 0,
         stretch: 390,
         depth: 150,
@@ -15,35 +13,5 @@ var swiper = new Swiper('.swiper-container.two', {
         slideShadows: false,
     },
     direction: 'horizontal',
-    mousewheel: {
-        forceToAxis: true,
-    },
-    touchEventsTarget: 'wrapper',
-    touchRatio: 1,
-    touchAngle: 45,
-    simulateTouch: true,
-    threshold: 5,
-});
-
-// Reverse direction for touch events
-swiper.on('touchStart', function (e) {
-    var touch = e.touches[0];
-    swiper.touchesStart.x = touch.pageX;
-    swiper.touchesStart.y = touch.pageY;
-});
-
-swiper.on('touchMove', function (e) {
-    var touch = e.touches[0];
-    var diffX = swiper.touchesStart.x - touch.pageX;
-    var diffY = swiper.touchesStart.y - touch.pageY;
-    
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-        if (diffX > 0) {
-            swiper.slidePrev();
-        } else {
-            swiper.slideNext();
-        }
-    }
-    
-    e.preventDefault();
+    reverseDirection: true
 });
