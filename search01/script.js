febd23517def414582ee5143f91761c9947eb737
vipulchaudhary31 <77533317+vipulchaudhary31@users.noreply.github.com>
2024-10-08 23:12:48 +0530

    Update script.js

--- search01/script.js
+++ search01/script.js
@@ -1,15 +1,49 @@
-var swiper = new Swiper( '.swiper-container.two', {
-		pagination: '.swiper-pagination',
-		paginationClickable: true,
-		effect: 'coverflow',
-		loop: false,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		coverflow: {
-			rotate:0		,
-			stretch: 390,
-			depth: 150,
-			modifier: 1.5,
-			slideShadows : false,
-		}
-} );
+var swiper = new Swiper('.swiper-container.two', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    effect: 'coverflow',
+    loop: false,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 390,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    },
+    direction: 'horizontal',
+    mousewheel: {
+        forceToAxis: true,
+    },
+    touchEventsTarget: 'wrapper',
+    touchRatio: 1,
+    touchAngle: 45,
+    simulateTouch: true,
+    threshold: 5,
+});
+
+// Reverse direction for touch events
+swiper.on('touchStart', function (e) {
+    var touch = e.touches[0];
+    swiper.touchesStart.x = touch.pageX;
+    swiper.touchesStart.y = touch.pageY;
+});
+
+swiper.on('touchMove', function (e) {
+    var touch = e.touches[0];
+    var diffX = swiper.touchesStart.x - touch.pageX;
+    var diffY = swiper.touchesStart.y - touch.pageY;
+    
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+        if (diffX > 0) {
+            swiper.slidePrev();
+        } else {
+            swiper.slideNext();
+        }
+    }
+    
+    e.preventDefault();
+});
